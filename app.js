ded4243db965963d94c60b864b6d99dca60ac0f1
RonakChawla <79718117+RonakChawla@users.noreply.github.com>
2021-06-10 13:50:51 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const searchInputBox = document.getElementById('input-box');
 searchInputBox.addEventListener('keypress', function(event){
     if(event.keyCode == 13){
         getWeatherReport(searchInputBox.value);
-        document.querySelector('.weather-body').style.display = "block";
     }
 })
 
@@ -21,40 +20,52 @@ function getWeatherReport(city){
 }
 
 function showWeatherReport(weather){
-    if(weather.message === 'city not found'){
-        alert("The weather for this city is currently unavailable. Please enter some nearby city.");
+    if(document.getElementById('input-box').value !== ''){
+        document.querySelector('.weather-body').style.display = "block";
+
+        if(weather.message === 'city not found'){
+            alert("The weather for this city is currently unavailable. Please enter some nearby city.");
+            document.querySelector('.weather-body').style.display = "none";
+        }
+    
+        document.getElementById('city').innerHTML = `${weather.name}, ${weather.sys.country}`;
+        document.getElementById('temp').innerHTML = `${weather.main.temp}&deg;C`;
+        document.getElementById('humidity').innerHTML = `Humidity: ${weather.main.humidity}`;
+    
+        let waetherType = document.getElementById('weather');
+        waetherType.innerHTML = `${weather.weather[0].main}`;
+    
+        let date = document.getElementById('date');
+        let todayDate = new Date();
+        date.innerText = dateManage(todayDate);
+    
+        if(waetherType.textContent === 'Clear'){
+            document.body.style.backgroundImage = "url('images/clear.jpg')";
+        } else if(waetherType.textContent === 'Clouds' || waetherType.textContent === 'Haze'){
+            document.body.style.backgroundImage = "url('images/clouds.jpg')";
+        } else if(waetherType.textContent === 'Sunny'){
+            document.body.style.backgroundImage = "url('images/sunny.jpg')";
+        } else if(waetherType.textContent === 'Rain'){
+            document.body.style.backgroundImage = "url('images/rain.jpg')";
+        } else if(waetherType.textContent === 'Snow'){
+            document.body.style.backgroundImage = "url('images/snow.jpg')";
+        } else if(waetherType.textContent === 'Thunderstorm'){
+            document.body.style.backgroundImage = "url('images/thunderstorm.jpg')";
+        }
+    
+        let cityName = `${weather.name[0].toUpperCase()}` + `${weather.name.slice(1)}`;
+    
+        if(cityName === `${weather.name}`){
+            showAlert("Results updated...");
+        }
+    } else {
         document.querySelector('.weather-body').style.display = "none";
     }
 
-    document.getElementById('city').innerHTML = `${weather.name}, ${weather.sys.country}`;
-    document.getElementById('temp').innerHTML = `${weather.main.temp}&deg;C`;
-    document.getElementById('min-max').innerHTML = `${weather.main.temp_min}&deg;C(min) / ${weather.main.temp_max}&deg;C(max)`;
-    let waetherType = document.getElementById('weather');
-    waetherType.innerHTML = `${weather.weather[0].main}`;
+}
 
-    let date = document.getElementById('date');
-    let todayDate = new Date();
-    date.innerText = dateManage(todayDate);
-
-    if(waetherType.textContent === 'Clear'){
-        document.body.style.backgroundImage = "url('images/clear.jpg')";
-    } else if(waetherType.textContent === 'Clouds' || waetherType.textContent === 'Haze'){
-        document.body.style.backgroundImage = "url('images/clouds.jpg')";
-    } else if(waetherType.textContent === 'Sunny'){
-        document.body.style.backgroundImage = "url('images/sunny.jpg')";
-    } else if(waetherType.textContent === 'Rain'){
-        document.body.style.backgroundImage = "url('images/rain.jpg')";
-    } else if(waetherType.textContent === 'Snow'){
-        document.body.style.backgroundImage = "url('images/snow.jpg')";
-    } else if(waetherType.textContent === 'Thunderstorm'){
-        document.body.style.backgroundImage = "url('images/thunderstorm.jpg')";
-    }
-
-    let cityName = `${weather.name[0].toUpperCase()}` + `${weather.name.slice(1)}`;
-
-    if(cityName === `${weather.name}`){
-        showAlert("Results updated...");
-    }
+function showMaxMin(weather){
+    console.log(weather);
 }
 
 function dateManage(dateArg){
@@ -73,13 +84,14 @@ function showAlert(message){
     clearAlert();
 
     const div = document.createElement('div');
+
     div.className = 'alert-msg';
 
     div.appendChild(document.createTextNode(message));
 
     const container = document.querySelector('.app-main');
 
-    const search = document.getElementById('weather-body');
+    const search = document.querySelector('.form-end');
 
     container.insertBefore(div, search);
 
